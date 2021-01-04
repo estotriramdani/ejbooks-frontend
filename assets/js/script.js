af54cd2c72ec0c9be2e69916ba7e74c7bd24a573
Esto Triramdani N <estotriramdani@gmail.com>
2021-01-04 19:10:26 +0700

    update header-navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
 const btnMenu = document.getElementById("btn-menu");
 const headerBody = document.getElementById("header-body");
 const contentBody = document.getElementsByClassName("content-body")[0];
+const hideHeader = document.getElementById("hide-header");
 
 btnMenu.addEventListener("click", function () {
   if (headerBody.classList.contains("show")) {
     headerBody.classList.remove("show");
+    hideHeader.classList.remove("clear");
   } else {
     headerBody.classList.add("show");
+    hideHeader.classList.add("clear");
   }
+});
+
+hideHeader.addEventListener("click", () => {
+  headerBody.classList.remove("show");
+  hideHeader.classList.remove("clear");
 });
